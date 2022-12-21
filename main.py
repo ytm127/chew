edfbdd1957be7275c62ff71f484109ef42de6ef4
Thomas Morita <thomasmorita@thomass-air.myfiosgateway.com>
2022-12-20 23:54:24 -0500

    add slug generation

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+from random import *
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
+def get_slug():
+    with open('5_letter_words.txt') as f:
+        lines = f.readlines()
+        n1 = randint(1, 5000)
+        n2 = randint(1, 5000)
+        n3 = randint(1, 5000)
+        return f"{lines[n1][0:5]}-{lines[n2][0:5]}"
+
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    slug = get_slug()
+    return {"message": f"Hello World, {slug}"}
 
 @app.get("/about")
 async def about():
